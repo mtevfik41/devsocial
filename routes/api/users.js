ed645ae2f447eac7afa5da4452e17c0aafceef90
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-10-14 23:59:11 +0300

    added jwt auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+const config = require('config');
+const {use} = require('express/lib/router');
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.get('/', (req, res) => {
-  res.send('3');
-});
 router.post(
   '/',
   [
@@ -33,7 +33,7 @@ router.post(
       // See if user exists
       let user = await User.findOne({email});
       if (user) {
-        res.status(400).json({
+        return res.status(400).json({
           errors: [{msg: 'User already exists'}],
         });
       }
@@ -58,7 +58,18 @@ router.post(
       await user.save();
       // Return jsonwebtoken
 
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, config.get('jwtSecret'), {
+        expiresIn: 360000,
+      }, (err, token) => {
+        if (err) throw err;
+        res.json({token});
+      });
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
