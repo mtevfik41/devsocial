4d9640f4a080de9a45fe5fde9a9c5c45f3cdf441
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-10-15 02:26:56 +0300

    typo fix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');git
+const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 const {check, validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
