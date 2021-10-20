d9947c31c9aaf23be0f80b58398553db2059d7c1
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-10-20 22:54:24 +0300

    react frontend

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path="/" component={ Landing } />
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
