91780ced74c4fc5e7e7b35fd6abc4ee30e327919
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2021-10-27 02:00:04 +0300

    updating reducers & components & actions with new features

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
   AUTH_ERROR,
+  CLEAR_PROFILE,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   LOGOUT,
@@ -93,5 +94,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / Clear Profile
 export const logout = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
